918adf7907ab2e5d86da0923a3e28c78649de8ad
yukesu26 <yusuke.562139@gmail.com>
2021-02-02 11:24:25 +0900

    ログイン、ログアウト、トップページ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attachment :profile_image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
    devise_for :users
   root to: "recette#index"
-end
+  resources :users
+ end
