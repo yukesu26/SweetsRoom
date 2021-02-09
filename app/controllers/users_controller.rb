b1f3d9c4d7c035a0eb06d9dafbf49674a57d5f18
yukesu26 <yusuke.562139@gmail.com>
2021-02-09 14:38:16 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+  end
+  
+
   private
     def user_params
       params.require(:user).permit(:user_name, :email, :profile, :profile_image)
