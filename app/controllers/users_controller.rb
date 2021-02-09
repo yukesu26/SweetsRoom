2322b366d915ac6b65e9bce0d874eec480e61dcf
yukesu26 <yusuke.562139@gmail.com>
2021-02-09 11:53:47 +0900

    ボタン、imageサイズ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @users = User.all
@@ -11,11 +12,18 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to root_path, alert: '不正なアクセスです!!!!!'
+    end
   end
+
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user)
+    if @user.update(user_params)
+    redirect_to user_path(@user), notice: '更新しました'
+    else
+      render :edit
+    end
   end
 
   private
